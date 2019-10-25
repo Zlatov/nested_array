784a121dd18007e7e7d113f03c665eb39aa9b018
zlatov <zlatov@ya.ru>
2019-10-25 11:17:49 +0300

    bundle gem ./

---
+++ lib/nested_array.rb
@@ -0,0 +1,6 @@
+require "nested_array/version"
+
+module NestedArray
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nested_array/version.rb
@@ -0,0 +1,3 @@
+module NestedArray
+  VERSION = "0.1.0"
+end
